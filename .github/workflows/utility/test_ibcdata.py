091c0dccc5be2320c735fbd51fedee4dbd181b5a
Dan Kanefsky <daniel.kanefsky@gmail.com>
2022-05-24 12:01:16 -0700

    add folder validation

--- .github/workflows/utility/test_ibcdata.py
+++ .github/workflows/utility/test_ibcdata.py
@@ -1,21 +1,21 @@
 import re
 import json
 from os import getcwd, listdir
-from os.path import isfile, join
+from os.path import isfile, isdir, join
 
 import pytest
 
 mypath = join(getcwd(),"_IBC")
-fileList = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+ibcData_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-@pytest.mark.parametrize("input", fileList)
+@pytest.mark.parametrize("input", ibcData_files)
 def test_fileName(input):
     # validates that the json file name has two "strings" separated by a hyphen (-) and ends with ".json"
     pattern = re.compile(r'.*-.*.json$')
     result = re.match(pattern, input)
     assert result
 
-@pytest.mark.parametrize("input", fileList)
+@pytest.mark.parametrize("input", ibcData_files)
 def test_alphabeticalOrder(input):
     # validates that chain_1 and chain_2 in file name are in alphabetical order
     pattern = re.compile(r'(.*)-(.*).json$')
@@ -24,7 +24,7 @@ def test_alphabeticalOrder(input):
     toSort.sort(key=str.lower)
     assert (m.group(1) == toSort[0]) and (m.group(2) == toSort[1])
 
-@pytest.mark.parametrize("input", fileList)
+@pytest.mark.parametrize("input", ibcData_files)
 def test_chainNameMatchFileName(input):
     # validates that the chain-name for chain-1 and chain-2 inside the json file match the order used in the file name.
     pattern = re.compile(r'(.*)-(.*).json$')
@@ -36,3 +36,12 @@ def test_chainNameMatchFileName(input):
         chain_1 = str(json_file["chain-1"]["chain-name"]).lower()
         chain_2 = str(json_file["chain-2"]["chain-name"]).lower()
     assert fileName_chain1 == chain_1 and fileName_chain2 == chain_2
+
+@pytest.mark.parametrize("input", ibcData_files)
+    # validates that the chain-name's used exist as root folders on the chain-registry
+def test_existstsOnChainReg(input):
+    pattern = re.compile(r'(.*)-(.*).json$')
+    m = pattern.match(input)
+    chain1 = m.group(1).lower()
+    chain2 = m.group(2).lower()
+    assert isdir(join(getcwd(),chain1)) and isdir(join(getcwd(),chain2))
