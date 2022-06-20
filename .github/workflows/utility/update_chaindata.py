c988101d00eb0f1b2f123418719b582a01d20d82
Xiangan He <xiangan9@gmail.com>
2022-06-20 11:13:46 -0400

    adding some inline docs

--- .github/workflows/utility/update_chaindata.py
+++ .github/workflows/utility/update_chaindata.py
@@ -11,16 +11,19 @@ def checkUpdate():
         # if os.path.isfile(chainjson):
         if chainjson == "osmosis/chain.json":
             current = json.load(open(os.path.join(rootdir, chainjson)))
-            
+        
+        #Safeguard for updatelink being 0
             if current['updatelink'] == None:
                 continue
-            
+        
             URL = current["updatelink"]
             chain_data_holder = requests.get("" + URL + "")
             response = json.loads(chain_data_holder.text)
             chaindata = response["codebase"]
-            
+        
+        #If what's on the chain repo doesn't match what's here
             if sorted(chaindata) != sorted(current["codebase"]):
+                #Add conditional checkers for if various fields that are non-modifiable have been modified.
                 current["codebase"] = chaindata
                 with open(os.path.join(rootdir, chainjson), 'w', encoding='utf-8') as f:
                     json.dump(current["codebase"], f, ensure_ascii=False, indent=4)
