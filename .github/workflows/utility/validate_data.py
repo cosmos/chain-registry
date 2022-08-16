b90698ff52808e16bf531d91663477144b7abf11
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2022-08-16 11:33:28 -0600

    Cleanup comments

--- .github/workflows/utility/validate_data.py
+++ .github/workflows/utility/validate_data.py
@@ -5,45 +5,24 @@ from os import getcwd
 
 rootdir = getcwd()
 
-
-
 checkSlip173 = 1
 slipWebsites = {}
 slipMainnetPrefixes = {}
 slipTestnetPrefixes = {}
 
-
 def readSLIP173():
-
-    # read slip1703 from file
-    #f = open("https://raw.githubusercontent.com/satoshilabs/slips/master/slip-0173.md", "r")
     slip173URL = "https://raw.githubusercontent.com/satoshilabs/slips/master/slip-0173.md"
-    # print(f.read())
     lines = []
     for line in urllib.request.urlopen(slip173URL):
       line = line.decode('utf-8')
       if (len(line) > 2):
-        # if (line[0:2] == "| ["):
         if (line[0] == "|" and line[2] == "["):
-          # print(line)
           lines.append(line)
-    #f.close()
-
-
     if lines:
       for line in lines:
-
-        # get name (pretty_name)
         pretty = line[3:line.find("]")]
-        # print(pretty)
-
-        # get website
         website = line[line.find("(")+1:line.find(")")]
-        # print(website)
-
         slipWebsites[pretty] = website
-
-        # get mainnet prefix
         secondPipe = line.find("|", 1)
         thirdPipe = line.find("|", secondPipe + 1)
         mainnetArea = line[secondPipe:thirdPipe]
@@ -52,14 +31,11 @@ def readSLIP173():
           secondQuote = mainnetArea.find("`", firstQuote + 1)
           if(secondQuote > 0):
             mainnetPrefix = mainnetArea[firstQuote + 1:secondQuote]
-            # print(mainnetPrefix)
             slipMainnetPrefixes[pretty] = mainnetPrefix
           else:
             print("Mainnet Bech32 Prefix undefined - missing second quote")
         else:
           print("Mainnet Bech32 Prefix undefined")
-
-        # get testnet prefix
         fourthPipe = line.find("|", thirdPipe + 1)
         testnetArea = line[thirdPipe:fourthPipe]
         firstQuote = testnetArea.find("`")
@@ -67,17 +43,9 @@ def readSLIP173():
           secondQuote = testnetArea.find("`", firstQuote + 1)
           if(secondQuote > 0):
             testnetPrefix = testnetArea[firstQuote + 1:testnetArea.find("`", firstQuote + 1)]
-            # print(testnetPrefix)
             slipTestnetPrefixes[pretty] = testnetPrefix
-            checkSlip173 = 1
           else:
             print("Mainnet Bech32 Prefix undefined - missing second quote")
-
-        # get regnet prefix
-        # fifthPipe = line.find("|", fourthPipe + 1)
-        # regnetArea = line[fourthPipe:fifthPipe]
-        # firstQuote = regnetArea.find("`")
-
     else:
       raise Exception("no SLIP-0173 entries recorded")
 
@@ -87,85 +55,44 @@ slipCoinTypesByName = {}
 slip44Websites = {}
 
 def readSLIP44():
-
-    # read slip44 from file
-    #f = open("https://raw.githubusercontent.com/satoshilabs/slips/master/slip-0044.md", "r")
     slip44URL = "https://raw.githubusercontent.com/satoshilabs/slips/master/slip-0044.md"
-    # print(f.read())
     lines = []
     for line in urllib.request.urlopen(slip44URL):
       line = line.decode('utf-8')
       if(len(line) > 6):
         if(line[0] != "-" and line[0] != "C" and (line[5] == "|" or line[6] == "|" or line[7] == "|" or line[8] == "|" or line[9] == "|" or line[10] == "|" or line[11] == "|")):
-          # print(line)
           lines.append(line)
-    #f.close()
-
-
-
     if lines:
       for line in lines:
-
-        # get coin number
         coinNumber = int(line[0:line.find(" ")])
-        # print(coinNumber)
-
-        # get name (pretty_name)
         if(line.find("[") > 0):
           pretty = line[line.find("[")+1:line.find("]")]
-          # print(pretty)
           website = line[line.find("(")+1:line.find(")")]
-          # print(website)
           slip44Websites[pretty] = website
-          checkSlip44 = 0
         else:
           firstPipe = line.find("|")
           secondPipe = line.find("|", firstPipe + 1)
           thirdPipe = line.find("|", secondPipe + 1)
           pretty = line[thirdPipe+2:len(line)-1]
-          # print(pretty)
         slipCoinTypesByNum[coinNumber] = pretty
         slipCoinTypesByName[pretty] = coinNumber
-
     else:
       raise Exception("no SLIP-0044 entries recorded")
 
-    # -----FOR EACH CHAIN-----
-
+# -----FOR EACH CHAIN-----
 def checkChains():
     for chainfolder in os.listdir(rootdir):
-
-        # read chain schema from file
-        #f = open("chain.json", "r")
-        #chainSchemaText = f.read()
-        #f.close()
-        
         chainjson = os.path.join(chainfolder, "chain.json")
         print(chainjson + "  - " + str(os.path.exists(chainjson)))
         if not os.path.exists(chainjson):
             continue
         chainSchema = json.load(open(os.path.join(rootdir, chainjson)))
-        
-
-        # turn json object into python object
-        #chainSchema = json.loads(chainSchemaText)
-
-        # read assetlist schema from file
-        #f = open("assetlist.json", "r")
-        #assetlistSchemaText = f.read()
-        #f.close()
-
-        # turn json object into python object
-        #assetlistSchema = json.loads(assetlistSchemaText)
-        
         assetlistjson = os.path.join(chainfolder, "assetlist.json")
         print(assetlistjson + "  - " + str(os.path.exists(assetlistjson)))
         if not os.path.exists(assetlistjson):
             continue
         assetlistSchema = json.load(open(os.path.join(rootdir, assetlistjson)))
-
         bases = []
-
         if "assets" in assetlistSchema:
           if assetlistSchema["assets"]:
             for asset in assetlistSchema["assets"]:
@@ -200,7 +127,6 @@ def checkChains():
             raise Exception("'assets' array doesn't contain any tokens")
         else:
           raise Exception("assetlist schema doesn't contain 'assets' array")
-
         if "fees" in chainSchema:
           if "fee_tokens" in chainSchema["fees"]:
             if chainSchema["fees"]["fee_tokens"]:
@@ -216,7 +142,6 @@ def checkChains():
             raise Exception("'fees' object doesn't contain 'fee_tokens' array")
         else:
           print("[OPTIONAL - Keplr Compliance] chain schema doesn't contain 'fees' object")
-
         if "staking" in chainSchema:
           if "staking_tokens" in chainSchema["staking"]:
             if chainSchema["staking"]["staking_tokens"]:
@@ -232,7 +157,6 @@ def checkChains():
             raise Exception("'fees' object doesn't contain 'staking_tokens' array")
         else:
           print("[OPTIONAL - Keplr Compliance] chain schema doesn't contain 'staking' object")
-
         if "network_type" in chainSchema:
           networkType = chainSchema["network_type"]
           if networkType == "mainnet":
@@ -243,7 +167,6 @@ def checkChains():
             raise Exception("network type unknown (not Mainnet nor Testnet)")
         else:
           raise Exception("chain schema doesn't contain 'network_type'")
-        
         if "pretty_name" in chainSchema:
           prettyName = chainSchema["pretty_name"]
           if checkSlip173:
@@ -274,9 +197,7 @@ def checkChains():
               print("[OPTIONAL - Keplr Compliance] chain schema doesn't contain 'slip44' string")
         else:
           raise Exception("chainSchema does not contain 'pretty_name'")
-
     print("Done")
-    
     
 def runAll():
   if checkSlip173:
