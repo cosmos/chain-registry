5794e7f006a1b13c0b8e87c5d054307394a562ab
Master Pi <minh.vln140501@gmail.com>
2023-05-16 12:51:56 +0700

    fix ci

--- .github/workflows/utility/upgrades/getUpgrades.py
+++ .github/workflows/utility/upgrades/getUpgrades.py
@@ -11,75 +11,76 @@ headers = {
 
 chainsData = {}
 chainsUpgrade = {}
-chainsList = []
+chainsList = ["a", "b", "c", "d", "e", "f"]
 
 def getUpgrades():
-    chainsFolders = sorted(os.listdir(rootdir))
-    for chainfolder in chainsFolders:
-        if chainfolder == "microtick":  # Microtick doesn't have endpoints
-            continue
-        chainjson = os.path.join(chainfolder, "chain.json")
-        if os.path.isfile(chainjson):
-            current = json.load(open(os.path.join(rootdir, chainjson)))
-            print(chainfolder)
-            rpcs = [items["address"] for items in current["apis"]["rpc"]]
-            apis = [items["address"] for items in current["apis"]["rest"]]
-            for item in apis:
-                # try:
-                #     match chainfolder:
-                #         case "gravitybridge":
-                #             url = f"{item}/blocks/latest"
-                #         case _:
-                #             url = f"{item}/cosmos/base/tendermint/v1beta1/blocks/latest"
-                #     block = requests.get(url, headers=headers)
-                #     blockHeight = int(block.json()['block']['header']['height'])
-                #     print(f"Current block height of {chainfolder}: {blockHeight}")
-                #     match chainfolder:
-                #         case "kyve" | "mars" | "quicksilver":
-                #             url = f"{item}/cosmos/gov/v1/proposals?pagination.reverse=true"
-                #         case _:
-                #             url = f"{item}/cosmos/gov/v1beta1/proposals?pagination.reverse=true"
-                #     data = requests.get(url, headers=headers)
-                #     data = data.json()['proposals']
-                #     for gov in data:
-                #         if gov["status"] == "PROPOSAL_STATUS_PASSED" or gov["status"] == "PROPOSAL_STATUS_VOTING_PERIOD":
-                #             if gov["content"]["@type"] == '/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal':
-                #                 if int(gov["content"]["plan"]["height"]) > blockHeight:
-                #                     chainsUpgrade[chainfolder] = {
-                #                         "height": int(gov["content"]["plan"]["height"]),
-                #                         "version": gov["content"]["plan"]["name"]
-                #                     }
-                #                     break
-                #     break
-                # except Exception as e:
-                #     print(f"Issue with request to {chainfolder}: {e}")
-                #     traceback.print_exc()
-                #     continue
+    # chainsFolders = sorted(os.listdir(rootdir))
+    # for chainfolder in chainsFolders:
+    #     if chainfolder == "microtick":  # Microtick doesn't have endpoints
+    #         continue
+    #     chainjson = os.path.join(chainfolder, "chain.json")
+    #     if os.path.isfile(chainjson):
+    #         current = json.load(open(os.path.join(rootdir, chainjson)))
+    #         print(chainfolder)
+    #         rpcs = [items["address"] for items in current["apis"]["rpc"]]
+    #         apis = [items["address"] for items in current["apis"]["rest"]]
+    #         for item in apis:
+    #             # try:
+    #             #     match chainfolder:
+    #             #         case "gravitybridge":
+    #             #             url = f"{item}/blocks/latest"
+    #             #         case _:
+    #             #             url = f"{item}/cosmos/base/tendermint/v1beta1/blocks/latest"
+    #             #     block = requests.get(url, headers=headers)
+    #             #     blockHeight = int(block.json()['block']['header']['height'])
+    #             #     print(f"Current block height of {chainfolder}: {blockHeight}")
+    #             #     match chainfolder:
+    #             #         case "kyve" | "mars" | "quicksilver":
+    #             #             url = f"{item}/cosmos/gov/v1/proposals?pagination.reverse=true"
+    #             #         case _:
+    #             #             url = f"{item}/cosmos/gov/v1beta1/proposals?pagination.reverse=true"
+    #             #     data = requests.get(url, headers=headers)
+    #             #     data = data.json()['proposals']
+    #             #     for gov in data:
+    #             #         if gov["status"] == "PROPOSAL_STATUS_PASSED" or gov["status"] == "PROPOSAL_STATUS_VOTING_PERIOD":
+    #             #             if gov["content"]["@type"] == '/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal':
+    #             #                 if int(gov["content"]["plan"]["height"]) > blockHeight:
+    #             #                     chainsUpgrade[chainfolder] = {
+    #             #                         "height": int(gov["content"]["plan"]["height"]),
+    #             #                         "version": gov["content"]["plan"]["name"]
+    #             #                     }
+    #             #                     break
+    #             #     break
+    #             # except Exception as e:
+    #             #     print(f"Issue with request to {chainfolder}: {e}")
+    #             #     traceback.print_exc()
+    #             #     continue
             
-                try:
-                    data = requests.get(f"{item}/cosmos/upgrade/v1beta1/current_plan", headers=headers)
-                    data = data.json()['plan']
-                    if data != None:
-                        chainsUpgrade[chainfolder] = {
-                            "height": int(data["height"]),
-                            "version": data["name"]
-                        }
-                    break
-                except Exception as e:
-                    print(f"Issue with request to {chainfolder}: {e}")
-                    continue
+    #             try:
+    #                 data = requests.get(f"{item}/cosmos/upgrade/v1beta1/current_plan", headers=headers)
+    #                 data = data.json()['plan']
+    #                 if data != None:
+    #                     chainsUpgrade[chainfolder] = {
+    #                         "height": int(data["height"]),
+    #                         "version": data["name"]
+    #                     }
+    #                 break
+    #             except Exception as e:
+    #                 print(f"Issue with request to {chainfolder}: {e}")
+    #                 continue
             
-            chainsList.append(chainfolder)
-            chainsData[chainfolder] = {
-                "rpc": rpcs,
-                "api": apis,
-            }
-    with open(".github/workflows/utility/upgrades/chainsEndpoint.json", "w") as file:
-        json.dump(chainsData, file, indent=4)
+    #         chainsList.append(chainfolder)
+    #         chainsData[chainfolder] = {
+    #             "rpc": rpcs,
+    #             "api": apis,
+    #         }
+    # with open(".github/workflows/utility/upgrades/chainsEndpoint.json", "w") as file:
+    #     json.dump(chainsData, file, indent=4)
         
-    with open(".github/workflows/utility/upgrades/chainsUpgrade.json", "w") as file:
-        json.dump(chainsUpgrade, file, indent=4)
+    # with open(".github/workflows/utility/upgrades/chainsUpgrade.json", "w") as file:
+    #     json.dump(chainsUpgrade, file, indent=4)
     
     os.environ["UPGRADE_LIST"] = str(chainsList)
+    print(os.environ["UPGRADE_LIST"])
     
     return chainsUpgrade
