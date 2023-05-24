fdd428e8e7a9188f907ba61bdefd4045ed86ff0a
Master Pi <minh.vln140501@gmail.com>
2023-05-24 17:59:40 +0700

    New flow

--- .github/workflows/utility/upgrades/getUpgrades.py
+++ .github/workflows/utility/upgrades/getUpgrades.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from updateCodebase import addVersion, updateCodebase
 
 rootdir = os.getcwd()
 headers = {
@@ -8,12 +9,41 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
 }
 
-chainsUpgrade = {}
+chainsUpdated = []
 
-def getUpgrades():
+def getHeight(rpcs):
+    for rpc in rpcs:
+        try:
+            data = requests.get(f"{rpc}/block", headers=headers, timeout=10)
+            return int(data.json()['result']['block']['header']['height'])
+        except Exception as e:
+            continue
+    return -1            
+
+def getUpgrade(chain, apis):
+    for item in apis:
+        try:
+            data = requests.get(f"{item}/cosmos/upgrade/v1beta1/current_plan", headers=headers, timeout=10)
+            data = data.json()['plan']
+            if data != None:
+                print(f"Found upgrade of {chain}")
+                return {
+                    "height": int(data["height"]),
+                    "name": data["name"],
+                    "info": data["info"],
+                }
+            else:
+                return None
+        except Exception as e:
+            print(f"Issue with request to {chain}: {e}", flush=True)
+            continue
+    
+    return None
+
+def run():
     chainsFolders = sorted(os.listdir(rootdir))
     for chainfolder in chainsFolders:
-        if chainfolder == "microtick":  # Microtick doesn't have endpoints
+        if chainfolder == "microtick" or chainfolder == "ethos" or chainfolder == "logos" or chainfolder == "mythos" or chainfolder == "octa":  # Microtick doesn't have endpoints, the others dont have codebase
             continue
         chainjson = os.path.join(chainfolder, "chain.json")
         if os.path.isfile(chainjson):
@@ -21,44 +51,23 @@ def getUpgrades():
             print(chainfolder, flush=True)
             rpcs = [items["address"] for items in current["apis"]["rpc"]]
             apis = [items["address"] for items in current["apis"]["rest"]]
-            for item in apis:
-                try:
-                    data = requests.get(f"{item}/cosmos/upgrade/v1beta1/current_plan", headers=headers, timeout=10)
-                    data = data.json()['plan']
-                    if data != None:
-                        chainsUpgrade[chainfolder] = {
-                            "height": int(data["height"]),
-                            "name": data["name"],
-                            "info": data["info"],
-                            "rpc": rpcs,
-                            "api": apis
-                        }
-                        print(f"Found upgrades for {chainfolder}!", flush=True)
-                    else:
-                        print(f"No upgrades for {chainfolder}! Using the latest version ...", flush=True)
-                        if chainfolder != "ethos" and chainfolder != "gitopia" and chainfolder != "logos" and chainfolder != "mythos":
-                            latestVersion = current["codebase"]["versions"][-1]
-                            chainsUpgrade[chainfolder] = {
-                                "height": latestVersion.get("height"),
-                                "name": latestVersion["name"],
-                                "rpc": rpcs,
-                                "api": apis
-                            }
-                            if latestVersion["name"] != current["codebase"]["name"]
-                    break
-                except Exception as e:
-                    print(f"Issue with request to {chainfolder}: {e}", flush=True)
-                    if chainfolder != "ethos" and chainfolder != "gitopia" and chainfolder != "logos" and chainfolder != "mythos":
-                        latestVersion = current["codebase"]["versions"][-1]
-                        chainsUpgrade[chainfolder] = {
-                            "height": latestVersion.get("height"),
-                            "name": latestVersion["name"],
-                            "rpc": rpcs,
-                            "api": apis
-                        }
-                    continue
+            currentVersion = current["codebase"]["versions"][-1]
+            upgrade = getUpgrade(chainfolder, apis)
             
-    with open(".github/workflows/utility/upgrades/chainsUpgrade.json", "w") as file:
-        json.dump(chainsUpgrade, file, indent=4)
-
-getUpgrades()
+            if upgrade != None:
+                if upgrade["name"] != currentVersion["name"]:
+                    addVersion(chainfolder, upgrade)
+                    chainsUpdated.append(chainfolder)
+                else:
+                   continue
+            else:
+                upgradeHeight = currentVersion["height"]
+                currentHeight = getHeight(rpcs)
+                if currentHeight != -1:
+                    if currentHeight <= upgradeHeight:
+                        continue
+                    else:
+                        updateCodebase(chainfolder)
+                        chainsUpdated.append(chainfolder)
+    with open(".github/workflows/utility/upgrades/chainsUpdated.json", "w") as file:
+        json.dump(chainsUpdated, file, indent=4)
--- .github/workflows/utility/upgrades/updateCodebase.py
+++ .github/workflows/utility/upgrades/updateCodebase.py
@@ -1,7 +1,5 @@
-import requests
 import json
 import os
-import time
 
 rootdir = os.getcwd()
 headers = {
@@ -9,49 +7,26 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
 }
 
-def checkHeight(api, rpc, versionHeight):
-    try:
-        data = requests.get(f"{rpc}/block", headers=headers, timeout=10)
-        currentHeight = int(data.json()['result']['block']['header']['height'])
-        if versionHeight <= currentHeight:
-            return True
-        else: return False
-    except Exception as e:
+def addVersion(chain, newVersion):
+    chainjson = os.path.join(chain, "chain.json")
+    current = json.load(open(os.path.join(rootdir, chainjson), encoding='utf-8', errors='ignore'))
+    current["codebase"]["versions"].append(newVersion)
+    with open(os.path.join(rootdir, chainjson), "w") as file:
+        json.dump(current, file, indent=2, ensure_ascii=False)
 
-def getUpgrade(chain):
-    print(rootdir, flush=True)
-    with open(".github/workflows/utility/upgrades/chainsUpgrade.json", "r") as file:
-        data = json.load(file)
-        chainData = data[chain]
-        rpcs = chainData["rpc"]
-        upgradeHeight = chainData["height"]
-        for item in rpcs:
-            try:
-                while True:
-                    data = requests.get(f"{item}/block", headers=headers, timeout=10)
-                    currentHeight = int(data.json()['result']['block']['header']['height'])
-                    if currentHeight <= upgradeHeight:
-                        time.sleep(40 * 60)
-                    else:
-                        chainjson = os.path.join(chain, "chain.json")
-                        current = json.load(open(os.path.join(rootdir, chainjson), encoding='utf-8', errors='ignore'))
-                        codebase = current["codebase"]
-                        latestVersion = current["codebase"]["versions"][-1]
-                        
-                        for key, value in latestVersion.items():
-                            if key != "name" and key != "height" and key != "proposal" and key != "tag" and key != "next_version_name":
-                                codebase[key] = value
-                                
-                        current["codebase"] = codebase
-                        
-                        with open(os.path.join(rootdir, chainjson), "w") as file:
-                            json.dump(current, file, indent=2, ensure_ascii=False)
-                        return
-            except Exception as e:
-                print(f"Issue with request to {chain}: {e}", flush=True)
-                continue
+def updateCodebase(chain):
+    chainjson = os.path.join(chain, "chain.json")
+    current = json.load(open(os.path.join(rootdir, chainjson), encoding='utf-8', errors='ignore'))
+    currentCodebase = current["codebase"]
+    currentVersion = current["codebase"]["versions"][-1]
+    for key, value in currentVersion.items():
+        if key != "name" and key != "height" and key != "proposal" and key != "tag" and key != "next_version_name":
+            currentCodebase[key] = value
+    current["codebase"] = currentCodebase
+    with open(os.path.join(rootdir, chainjson), "w") as file:
+        json.dump(current, file, indent=2, ensure_ascii=False)
 
-def update_codebase():
+def updateCodebase1():
     chainsFolders = sorted(os.listdir(rootdir))
     for chainfolder in chainsFolders:
         if chainfolder == "ethos" or chainfolder == "logos" or chainfolder == "mythos" or chainfolder == "octa":
