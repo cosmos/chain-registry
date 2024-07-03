766b89e05b7cb4049933be361bf454ecae2d9b62
Sunny Aggarwal <sunnya97@protonmail.ch>
2024-07-03 21:47:44 +0100

    add network primary colors

--- primary_colors.py
+++ primary_colors.py
@@ -15,52 +15,58 @@ def get_primary_color(png):
     return "#%02x%02x%02x" % dominant_color
 
 
+def add_primary_color_to_image(image):
+    if "png" not in image.keys():
+        return
+
+    if "theme" in image.keys() and "primary_color_hex" in image["theme"].keys():
+        return
+
+    png = image["png"]
+    png = png.replace(
+        "https://raw.githubusercontent.com/cosmos/chain-registry/master",
+        ".",
+    )
+
+    try:
+        hex = get_primary_color(png)
+    except:
+        return
+
+    image.setdefault("theme", {})["primary_color_hex"] = hex
+
+    print(image["theme"]["primary_color_hex"])
+
+    return image
+
+
 for item in chain_registry.rglob("*"):
     if (
         item.is_file()
-        and "assetlist.json" in item.name
+        and ("assetlist.json" in item.name or "chain.json" in item.name)
         and "_template" not in item.parts
+        and "_IBC" not in item.parts
         and "testnets" not in item.parts
     ):
         with open(item, "r+", encoding="utf-8") as f:
             data = json.load(f)
-            for asset in data["assets"]:
 
-                print(asset["symbol"])
+            if "images" in data.keys():
+                for image in data["images"]:
+                    new_image = add_primary_color_to_image(image)
+                    if new_image:
+                        image = new_image
 
-                if "images" not in asset.keys():
-                    continue
+            if "assets" in data.keys():
+                for asset in data["assets"]:
 
-                for i in range(len(asset["images"])):
+                    print(asset["symbol"])
 
-                    if "png" not in asset["images"][i].keys():
-                        continue
-
-                    if (
-                        "theme" in asset["images"][i].keys()
-                        and "primary_color_hex" in asset["images"][i]["theme"].keys()
-                    ):
-                        continue
-
-                    png = asset["images"][i]["png"]
-                    png = png.replace(
-                        "https://raw.githubusercontent.com/cosmos/chain-registry/master",
-                        ".",
-                    )
-
-                    try:
-                        hex = get_primary_color(png)
-                    except:
-                        continue
-
-                    asset.setdefault("images", [{}])[i].setdefault("theme", {})[
-                        "primary_color_hex"
-                    ] = hex
-
-                    print(asset["images"][i]["theme"]["primary_color_hex"])
+                    if "images" in asset.keys():
+                        for image in asset["images"]:
+                            new_image = add_primary_color_to_image(image)
+                            if new_image:
+                                image = new_image
 
             print(data)
             item.write_text(json.dumps(data, indent=2, ensure_ascii=False))
-
-
-# https://raw.githubusercontent.com/cosmos/chain-registry/master/osmosis/images/wosmo.png
