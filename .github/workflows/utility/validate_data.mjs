1ddd70572671fa6ee269d349b9a3682379a50acb
Jeremy Parish <jeremyparish69@gmail.com>
2024-11-19 11:43:42 -0700

    Update Slip44 for 11 chains

--- .github/workflows/utility/validate_data.mjs
+++ .github/workflows/utility/validate_data.mjs
@@ -45,7 +45,7 @@ function checkSlip44(chain_name) {
   if (!chain_status || chain_status === "upcoming" || chain_status === "killed") { return; }
   let slip44 = chain_reg.getFileProperty(chain_name, "chain", "slip44");
   if (!slip44) {
-    /*throw new Error*/console.log(`Chain ${chain_name} missing slip44!`);
+    throw new Error(`Chain ${chain_name} missing slip44!`);
   }
 
 }
