df3974a12bd3db4b69cf8d28a8759aacf204f2d0
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2025-02-17 13:13:49 -0700

    Write Coingecko ID Update Code + Bulk Update of `coingecko_id`s (#5941)
    * write coingecko id updatecode
    
    * bulk update of cgids

--- .github/workflows/utility/update_coingecko_data.mjs
+++ .github/workflows/utility/update_coingecko_data.mjs
@@ -89,6 +89,61 @@ Error: Missing Coingecko ID: ${coingecko_id} for asset`, asset);
   await coingecko.saveCoingeckoState();
 }
 
+async function addValidCoingeckoIds() { //run state update first
+
+  await coingecko.loadCoingeckoState();
+
+  if (!coingecko.state) { return; }
+
+
+  let STATE_UPDATED = false;
+  chain_reg.getAssetPointers().forEach((asset) => {
+
+    //get the cgid origin
+    const origin_asset = chain_reg.getOriginAsset(asset.chain_name, asset.base_denom, coingecko.traceTypesCoingeckoId);
+    if (!origin_asset) {
+      console.log("Cannot find origin asset.");
+      return;
+    }
+
+    //get the coingecko id group
+    const coingecko_id_group = coingecko.state.coingecko_id_groups?.find(a =>
+      a.origin_asset?.chain_name === origin_asset.chain_name &&
+      a.origin_asset?.base_denom === origin_asset.base_denom
+    );
+    if (!coingecko_id_group) { return; }
+
+    //see if this asset is already in the state::coingecko_id_group::assets array
+    if (!coingecko_id_group.assets) {
+      coingecko_id_group.assets = [];
+    }
+    const assetExists = coingecko_id_group.assets.some(groupAsset =>
+      groupAsset.chain_name === asset.chain_name &&
+      groupAsset.base_denom === asset.base_denom
+    );
+    if (assetExists) { return; }
+
+    //define old and new
+    const new_cgid = coingecko_id_group.coingecko_id;
+    const old_cgid = chain_reg.getAssetMetadata(asset.chain_name, asset.base_denom, "coingecko_id", []) ?? null;
+    if (old_cgid === new_cgid) { return; }
+
+    //update the coingecko id
+    console.log(`Updated Coingecko ID for asset: ${asset.chain_name} / ${asset.base_denom}`);
+    console.log(`Previous ID: ${old_cgid || "None"} -> New ID: ${new_cgid}`);
+    chain_reg.setAssetProperty(asset.chain_name, asset.base_denom, "coingecko_id", coingecko_id_group.coingecko_id);
+    //update the state
+    coingecko_id_group.assets.push(asset);
+    STATE_UPDATED = true;
+    
+  });
+
+  if (STATE_UPDATED) {
+    await coingecko.saveCoingeckoState();
+  }
+  
+}
+
 function main() {
   return;
 }
@@ -100,6 +155,8 @@ if (process.argv.length > 2) {
     generateCoingeckoStateFile();
   } else if (command === 'removeInvalidCoingeckoIds') {
     removeInvalidCoingeckoIds();
+  } else if (command === 'addValidCoingeckoIds') {
+    addValidCoingeckoIds();
   } else {
     console.log(`Unknown command: ${command}`);
   }
